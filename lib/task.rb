a242359e40a957f8d26d6ffa870d384f1e6adcfe
Mark Scheel <mscheel@itriagehealth.com>
2015-07-09 15:23:17 -0600

    all tests passing

--- lib/task.rb
+++ lib/task.rb
@@ -1,4 +1,20 @@
 class Task
-
-
+	# : signifies symbol, one location in memory entire time
+	attr_accessor :title
+	attr_accessor :priority
+	attr_accessor :completed
+	
+	def initialize(title, priority = 10)
+		self.title = title
+		self.priority = priority
+		self.completed = false
+	end
+	
+	def complete?()
+		completed
+	end
+	
+	def complete
+		self.completed = true;
+	end
 end
--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -2,6 +2,17 @@
 
 class TodoList
 
+	attr_accessor :tasks
+
+	def initialize()
+		self.tasks = []
+	end
+
+	def add(task)
+		# << means append
+		tasks << task
+	end
+
 end
 
 if __FILE__ == $0
